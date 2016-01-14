8eb3c8a607525cbe854302ef4c711a5e91d8803e
Kommanapalli Krishna <thespidy@Kommanapallis-MacBook-Pro.local>
2016-01-14 18:01:24 +0530

    created models

---
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/models/Address.java
@@ -0,0 +1,62 @@
+package com.cargoexchange.cargocity.cargocity.models;
+
+/**
+ * Created by thespidy on 14/01/16.
+ */
+public class Address {
+    private String houseNumber;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String pincode;
+    private String state;
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
---
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/models/Customer.java
@@ -0,0 +1,62 @@
+package com.cargoexchange.cargocity.cargocity.models;
+
+/**
+ * Created by thespidy on 14/01/16.
+ */
+public class Customer {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String primaryPhone;
+    private String secondaryPhone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrimaryPhone() {
+        return primaryPhone;
+    }
+
+    public void setPrimaryPhone(String primaryPhone) {
+        this.primaryPhone = primaryPhone;
+    }
+
+    public String getSecondaryPhone() {
+        return secondaryPhone;
+    }
+
+    public void setSecondaryPhone(String secondaryPhone) {
+        this.secondaryPhone = secondaryPhone;
+    }
+}
+
