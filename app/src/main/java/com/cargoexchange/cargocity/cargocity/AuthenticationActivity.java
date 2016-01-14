1462dd133676a55e5317e263352c630307487122
HP <somesh557@gmail.com>
2016-01-14 18:54:37 +0530

    login changes

--- app/src/main/java/com/cargoexchange/cargocity/cargocity/AuthenticationActivity.java
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/AuthenticationActivity.java
@@ -1,5 +1,7 @@
 package com.cargoexchange.cargocity.cargocity;
 
+import android.support.v4.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +11,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.cargoexchange.cargocity.cargocity.fragments.LoginFragment;
+
 public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Fragment fm = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(fm,"xyz").commit();
     }
 
     @Override
