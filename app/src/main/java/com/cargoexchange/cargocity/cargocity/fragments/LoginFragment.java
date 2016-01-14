f901f4c716efa133119ef79d6b93bc59c737c6ed
Kommanapalli Krishna <thespidy@Kommanapallis-MacBook-Pro.local>
2016-01-14 16:48:02 +0530

    initial commit

---
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/fragments/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.cargoexchange.cargocity.cargocity.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cargoexchange.cargocity.cargocity.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
