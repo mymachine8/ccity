7ee0525b38d9f2156848dbdb96b56e5888ff9603
Kommanapalli Krishna <thespidy@Kommanapallis-MacBook-Pro.local>
2016-01-18 12:18:01 +0530

    added document scanner

--- app/src/main/java/com/cargoexchange/cargocity/cargocity/fragments/FeedbackFragment.java
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/fragments/FeedbackFragment.java
@@ -1,18 +1,30 @@
 package com.cargoexchange.cargocity.cargocity.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.cargoexchange.cargocity.cargocity.R;
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
+
+import java.io.IOException;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FeedbackFragment extends Fragment {
-
+    private static final int CAMERA_REQUEST_CODE = 99;
+    private ImageView mCameraImage;
+    private Activity mActivityContext;
     public FeedbackFragment() {
     }
 
@@ -20,5 +32,38 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_delivery_feedback, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mActivityContext = getActivity();
+        ImageView mCameraImage = (ImageView)view.findViewById(R.id.image);
+        mCameraImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int preference = ScanConstants.OPEN_CAMERA;
+                Intent intent = new Intent(getActivity(), ScanActivity.class);
+                intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(mActivityContext.getContentResolver(), uri);
+                mActivityContext.getContentResolver().delete(uri, null, null);
+                mCameraImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
