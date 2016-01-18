f0a546464de0db6417fc76e5f29564904e876055
Kommanapalli Krishna <krishna.hermione@gmail.com>
2016-01-18 16:59:11 +0530

    Login functionality (sending data to server is pending)

---
+++ app/src/main/java/com/cargoexchange/cargocity/cargocity/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.cargoexchange.cargocity.cargocity.constants;
+
+/**
+ * Created by thespidy on 18/01/16.
+ */
+public class Constants {
+    public static final String CARGO_API_BASE_URL  = "http//cargoexchange.com/api/v1/";
+}
